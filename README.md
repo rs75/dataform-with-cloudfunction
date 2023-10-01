798696897919e49b920f5768d904e73cd4fe7a01
Your Name <you@example.com>
2023-10-01 20:38:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Trigger Dataform workflow with Google Cloud Function
+
+This is a simple example of how to trigger a Dataform workflow with a Google Cloud Function. The cloud function is triggered by a HTTP request. The cloud function then triggers a Dataform workflow by calling the Dataform API.
+For setup, execute the commands below in the given order to deploy the cloud function. Ensure you update the variables at the beginning of main.py
+
+#### Select project you want to deploy cloud function to
+
+```
+gcloud config set project <project-name>
+```
+
+#### Deploy cloud function
+
+```
+gcloud functions deploy trigger-dataform-function --gen2 --runtime=python311 --region=us-central1 --source=. --entry-point=trigger_dataform --trigger-http --allow-unauthenticated
+
+```
+
+It might ask you to enable apis, just say yes.
+Consider removing "--allow-unauthenticated" for enhanced security.
+
+
+
+#### Give service account of cloud function permissions for dataform 
+Easiest way is to give role "roles/dataform.editor"
+```
+gcloud projects add-iam-policy-binding <project-name> --member=serviceAccount:<service-account-email> --role=roles/dataform.editor
+```
+
+The service account is automatically created upon deploying the first cloud function. You can find the name of the service account under the "Details" tab in the cloud function console in GCP.
---
+++ main.py
@@ -0,0 +1,73 @@
+import requests
+import json
+import functions_framework
+from google.auth.transport.requests import Request
+import google.auth
+
+dataform_project = "cloudfunction-dataform-demo"
+dataform_location = "us-central1"
+dataform_repository = "test"
+dataform_workspace = "test"
+
+
+@functions_framework.http
+def trigger_dataform(request):
+    """
+    This function compiles and triggers a dataform workflow and return status.
+    """
+
+    # Create auth token for the api. Important: the service account that
+    # runs the cloud function must have dataform.editor role
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    credentials, project = google.auth.default(scopes=scopes)
+    # If necessary, refresh the credentials
+    if not credentials.valid:
+        credentials.refresh(Request())
+    api_auth_token = credentials.token
+    # print(api_auth_token)
+
+    api_url = "https://dataform.googleapis.com/v1beta1"
+    repository_path = f"projects/{dataform_project}/locations/{dataform_location}/repositories/{dataform_repository}"
+    api_compilation_url = f"{api_url}/{repository_path}/compilationResults"
+    api_workflow_invocations_url = f"{api_url}/{repository_path}/workflowInvocations"
+    workspace = f"{repository_path}/workspaces/{dataform_workspace}"
+
+    headers = {
+        "Authorization": f"Bearer {api_auth_token}",
+        "Content-Type": "application/json",
+    }
+
+    # Compile the dataform workflow. This is needed otherwise the workflow invocation will fail
+    print("try to compile worflow")
+    payload = json.dumps({"workspace": workspace})
+    response = requests.request(
+        "POST", api_compilation_url, data=payload, headers=headers
+    )
+    if response.status_code != 200:
+        raise Exception(
+            f"Request to {api_compilation_url} failed with status code {response.status_code}: {response.text}"
+        )
+    print(response.status_code)
+    print(response.json())
+    compliation_name = response.json()["name"]
+
+    # Trigger the dataform workflow
+    print("try to trigger workflow")
+    payload = json.dumps({"compilationResult": compliation_name})
+    response = requests.request(
+        "POST", api_workflow_invocations_url, headers=headers, data=payload
+    )
+    if response.status_code != 200:
+        raise Exception(
+            f"Request to {api_workflow_invocations_url} failed with status code {response.status_code}: {response.text}"
+        )
+    print(response.status_code)
+    response_json = response.json()
+    print(response_json)
+
+    return "Done - Dataform state is: " + response_json["state"]
+
+
+# uncomment for local testing
+# if __name__ == "__main__":
+#    trigger_dataform(None)
