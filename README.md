716451ed5b2bd73066c87fecb0b49d9f933f26f4
Your Name <you@example.com>
2023-10-04 21:25:17 +0200

    update

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Trigger Dataform workflow with Google Cloud Function
 
 This is a simple example of how to trigger a Dataform workflow with a Google Cloud Function. The cloud function is triggered by a HTTP request. The cloud function then triggers a Dataform workflow by calling the Dataform API.
+
+
+
+The function waits for the workflow to complete by iteratively checking its status. On completion, it will either return "success" or "timeout" based on the outcome. 
+By default the dataform workflow is not compiled. If you want to complile, add "/compile" to the end of the url. If you do this the path to the compiled model is logged in the cloud function console. You can use this value to update the "compiled_model_path" variable in main.py. 
+
+
 For setup, execute the commands below in the given order to deploy the cloud function. Ensure you update the variables at the beginning of main.py
 
 #### Select project you want to deploy cloud function to
@@ -28,3 +35,5 @@ gcloud projects add-iam-policy-binding <project-name> --member=serviceAccount:<s
 ```
 
 The service account is automatically created upon deploying the first cloud function. You can find the name of the service account under the "Details" tab in the cloud function console in GCP.
+
+
--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import requests
 import json
+import time
 import functions_framework
 from google.auth.transport.requests import Request
 import google.auth
+
 
 dataform_project = "cloudfunction-dataform-demo"
 dataform_location = "us-central1"
 dataform_repository = "test"
 dataform_workspace = "test"
 
+#variables for the status_chck
+initial_wait = 10
+wait_interval = 10
+wait_iterations  = 100
+
+
+# Don't forget to update this variable after updating model. This variable is not used if you call "/compile" endpoint
+compilation_result_path = "projects/cloudfunction-dataform-demo/locations/us-central1/repositories/test/compilationResults/a6887a3c-799a-4829-a8c2-bf0d2032fa2f"
+
 
 @functions_framework.http
 def trigger_dataform(request):
+
     """
     This function compiles and triggers a dataform workflow and return status.
     """
+    #Check the URL path
+    if 'compile' in request.path:
+        compile = True
+    else:
+        compile = False
+
+    print(".....compile: " + str(compile))
 
     # Create auth token for the api. Important: the service account that
     # runs the cloud function must have dataform.editor role
@@ -37,27 +56,33 @@ def trigger_dataform(request):
         "Content-Type": "application/json",
     }
 
-    # Compile the dataform workflow. This is needed otherwise the workflow invocation will fail
-    print("try to compile worflow")
-    payload = json.dumps({"workspace": workspace})
-    response = requests.request(
-        "POST", api_compilation_url, data=payload, headers=headers
-    )
-    if response.status_code != 200:
-        raise Exception(
-            f"Request to {api_compilation_url} failed with status code {response.status_code}: {response.text}"
+
+    if compile:
+
+        # Compile the dataform workflow. This is needed otherwise the workflow invocation will fail
+        print("try to compile worflow")
+        payload = json.dumps({"workspace": workspace})
+        response = requests.request(
+            "POST", api_compilation_url, data=payload, headers=headers
         )
-    print(response.status_code)
-    print(response.json())
-    compliation_name = response.json()["name"]
+        if response.status_code != 200:
+            raise Exception(
+                f"Request to {api_compilation_url} failed with status code {response.status_code}: {response.text}"
+            )
+        print(response.status_code)
+        print(response.json())
+        compliation_name = response.json()["name"]
+        print("###############################################")
+        print("Compilation result name: " + compliation_name)
+        print("###############################################")
+    else:
+        compliation_name= compilation_result_path
 
     # Trigger the dataform workflow
     print("try to trigger workflow")
-
     payload = json.dumps({"compilationResult": compliation_name})
     # Optional, you can add InvocationConfig to the payloud
-    # https://cloud.google.com/dataform/reference/rest/v1beta1/InvocationConfig
-    # e.g. payload = json.dumps({"compilationResult": compliation_name, "InvocationConfig" : { "includedTags": ["tag1"]}})
+    payload = json.dumps({"compilationResult": compliation_name, "invocationConfig": {"includedTags": ["tag1"]}})
 
     response = requests.request(
         "POST", api_workflow_invocations_url, headers=headers, data=payload
@@ -66,13 +91,38 @@ def trigger_dataform(request):
         raise Exception(
             f"Request to {api_workflow_invocations_url} failed with status code {response.status_code}: {response.text}"
         )
-    print(response.status_code)
+
     response_json = response.json()
+    invocation_id = response_json["name"]
+
+    print(response.status_code)
     print(response_json)
 
-    return "Done - Dataform state is: " + response_json["state"]
+    time.sleep(initial_wait)
 
+    print("Check status")
+
+    # Wait until the workflow is done
+    for i in range (wait_iterations):
+        response = requests.request(
+            "GET", api_workflow_invocations_url, headers=headers
+        )
+        if response.status_code != 200:
+            raise Exception(
+                f"Check status failed with status code {response.status_code}: {response.text}"
+            )
+        response_json = response.json()
+        for row in response_json["workflowInvocations"]:
+            if row["name"] == invocation_id:
+                if row["state"] == "SUCCEEDED":
+                    return "success"
+
+        time.sleep(wait_interval)
+
+    return "timeout"
 
 # uncomment for local testing
-# if __name__ == "__main__":
-#    trigger_dataform(None)
+#if __name__ == "__main__":
+#    print(trigger_dataform(None))
+
+
